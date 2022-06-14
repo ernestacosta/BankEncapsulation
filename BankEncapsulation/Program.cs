28a82213b254ffd71c552f7816842c043d978c0d
Ernest Acosta <ernestacosta@icloud.com>
2022-06-13 21:25:38 -0700

    initial commit

---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount();
+            Console.WriteLine("How much would you like to deposit?");
+            var amountToDeposit = double.Parse(Console.ReadLine());
+
+            account.Deposit(amountToDeposit);
+
+            Console.WriteLine($"Thank you! Your Balance is now {account.GetBalance()}");
+
+        }
+    }
+}
